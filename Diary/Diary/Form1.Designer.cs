f4fc06c011a927e977979a4a8f48bda2a3f31cde
Juhyung <juhyung649@gmail.com>
2023-10-05 21:17:34 +0900

    10/5

---
+++ Diary/Diary/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Diary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.TitleBox = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.새로만들기 = new System.Windows.Forms.ToolStripMenuItem();
+            this.열기 = new System.Windows.Forms.ToolStripMenuItem();
+            this.저장 = new System.Windows.Forms.ToolStripMenuItem();
+            this.다른이름으로저장 = new System.Windows.Forms.ToolStripMenuItem();
+            this.끝내기 = new System.Windows.Forms.ToolStripMenuItem();
+            this.서식OToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.글꼴FToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.MainTextBox = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.TitleBox);
+            this.groupBox1.Location = new System.Drawing.Point(13, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(268, 53);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "제목";
+            // 
+            // TitleBox
+            // 
+            this.TitleBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TitleBox.Location = new System.Drawing.Point(3, 17);
+            this.TitleBox.Name = "TitleBox";
+            this.TitleBox.Size = new System.Drawing.Size(262, 21);
+            this.TitleBox.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.dateTimePicker1);
+            this.groupBox2.Location = new System.Drawing.Point(369, 28);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(194, 52);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "날짜";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(3, 17);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(188, 21);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일ToolStripMenuItem,
+            this.서식OToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(855, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 파일ToolStripMenuItem
+            // 
+            this.파일ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.새로만들기,
+            this.열기,
+            this.저장,
+            this.다른이름으로저장,
+            this.끝내기});
+            this.파일ToolStripMenuItem.Name = "파일ToolStripMenuItem";
+            this.파일ToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.파일ToolStripMenuItem.Text = "파일 (&F)";
+            // 
+            // 새로만들기
+            // 
+            this.새로만들기.Name = "새로만들기";
+            this.새로만들기.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.새로만들기.Size = new System.Drawing.Size(272, 22);
+            this.새로만들기.Text = "새로만들기 (&N)";
+            this.새로만들기.Click += new System.EventHandler(this.새로만들기_Click);
+            // 
+            // 열기
+            // 
+            this.열기.Name = "열기";
+            this.열기.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.열기.Size = new System.Drawing.Size(272, 22);
+            this.열기.Text = "열기 (&O)";
+            this.열기.Click += new System.EventHandler(this.열기OToolStripMenuItem_Click);
+            // 
+            // 저장
+            // 
+            this.저장.Name = "저장";
+            this.저장.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.저장.Size = new System.Drawing.Size(272, 22);
+            this.저장.Text = "저장 (&S)";
+            this.저장.Click += new System.EventHandler(this.저장SToolStripMenuItem_Click);
+            // 
+            // 다른이름으로저장
+            // 
+            this.다른이름으로저장.Name = "다른이름으로저장";
+            this.다른이름으로저장.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+            this.다른이름으로저장.Size = new System.Drawing.Size(272, 22);
+            this.다른이름으로저장.Text = "다른 이름으로 저장 (&A)";
+            this.다른이름으로저장.Click += new System.EventHandler(this.다른이름으로저장ToolStripMenuItem_Click);
+            // 
+            // 끝내기
+            // 
+            this.끝내기.Name = "끝내기";
+            this.끝내기.Size = new System.Drawing.Size(272, 22);
+            this.끝내기.Text = "끝내기 (&X)";
+            this.끝내기.Click += new System.EventHandler(this.끝내기XToolStripMenuItem_Click);
+            // 
+            // 서식OToolStripMenuItem
+            // 
+            this.서식OToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.글꼴FToolStripMenuItem});
+            this.서식OToolStripMenuItem.Name = "서식OToolStripMenuItem";
+            this.서식OToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.서식OToolStripMenuItem.Text = "서식(&O)";
+            // 
+            // 글꼴FToolStripMenuItem
+            // 
+            this.글꼴FToolStripMenuItem.Name = "글꼴FToolStripMenuItem";
+            this.글꼴FToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.글꼴FToolStripMenuItem.Text = "글꼴(&F)";
+            this.글꼴FToolStripMenuItem.Click += new System.EventHandler(this.글꼴FToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.radioButton3);
+            this.groupBox3.Controls.Add(this.radioButton2);
+            this.groupBox3.Controls.Add(this.radioButton1);
+            this.groupBox3.Location = new System.Drawing.Point(579, 28);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(265, 52);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "날씨";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(224, 21);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(35, 16);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "눈";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(114, 20);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(35, 16);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "비";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 16);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "맑음";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // MainTextBox
+            // 
+            this.MainTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MainTextBox.Location = new System.Drawing.Point(12, 86);
+            this.MainTextBox.Multiline = true;
+            this.MainTextBox.Name = "MainTextBox";
+            this.MainTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.MainTextBox.Size = new System.Drawing.Size(831, 390);
+            this.MainTextBox.TabIndex = 4;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 479);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(855, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(855, 501);
+            this.Controls.Add(this.MainTextBox);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(871, 540);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox TitleBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 새로만들기;
+        private System.Windows.Forms.ToolStripMenuItem 열기;
+        private System.Windows.Forms.ToolStripMenuItem 저장;
+        private System.Windows.Forms.ToolStripMenuItem 다른이름으로저장;
+        private System.Windows.Forms.ToolStripMenuItem 끝내기;
+        private System.Windows.Forms.TextBox MainTextBox;
+        private System.Windows.Forms.ToolStripMenuItem 서식OToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 글꼴FToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ Diary/Diary/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Diary
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            TitleBox.Focus();
+
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+
+        }
+
+        string FilePath = "";
+        string FileName = "";
+        string Weather = "";
+
+
+        /* 할거
+         * 
+         * 폰트 가져오기
+         * 내용 가져오기
+         * 주석 달기
+         * 버튼마다 이벤트 만들어주기
+         * 일반일기랑 비밀일기 분할
+         * 저장 조건 및 열기 조건 추가
+         * 
+         */
+
+
+        private void 열기OToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "텍스트문서(*.txt)|*.txt";
+            openFileDialog1.Title = "열기";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FilePath = openFileDialog1.FileName;
+                FileName = Path.GetFileNameWithoutExtension(FilePath);
+
+                TextBox openFileData = new TextBox();
+                openFileData.Text = System.IO.File.ReadAllText(openFileDialog1.FileName);
+
+                SetFileData(openFileData);
+
+                this.Text = FileName;
+            }
+
+        }
+
+        private void 저장SToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (FilePath == "")
+            {
+                saveFileDialog1.Filter = "텍스트문서(*.txt)|*.txt";
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    FilePath = saveFileDialog1.FileName;
+                    FileName = Path.GetFileNameWithoutExtension(FilePath);
+                    saveFileDialog1.FileName = FileName;
+
+                    TextBox SaveDate = new TextBox();
+                    SaveDate.Text =
+                    TitleBox.Text + "\r\n" +
+                    Weather + "\r\n" +
+                    dateTimePicker1.Value.ToString() + "\r\n" +
+                    MainTextBox.Font.ToString() + "\r\n" +
+                    MainTextBox.Text;
+
+                    System.IO.File.WriteAllText(FilePath, SaveDate.Text);
+
+                    this.Text = FileName;
+                }
+            }
+        }
+
+        private void 다른이름으로저장ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "텍스트문서(*.txt)|*.txt";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FilePath = saveFileDialog1.FileName;
+                FileName = Path.GetFileNameWithoutExtension(FilePath);
+
+                TextBox SaveDate = new TextBox();
+                SaveDate.Text = 
+                    TitleBox.Text + "\r\n" + 
+                    Weather + "\r\n" +
+                    dateTimePicker1.Value.ToString() + "\r\n" + 
+                    MainTextBox.Font.ToString() + "\r\n" + 
+                    MainTextBox.Text;
+
+                System.IO.File.WriteAllText(FilePath, SaveDate.Text);
+
+                this.Text = FileName;
+            }
+        }
+
+        private void 끝내기XToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 글꼴FToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowDialog();
+            MainTextBox.Font = fontDialog1.Font;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            Weather = "Sunny";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            Weather = "Rainy";
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            Weather = "Snow";
+        }
+
+        private void SetFileData(TextBox openFileData)
+        {
+            TitleBox.Text = openFileData.Lines[0].ToString();
+
+            // 날씨 설정
+            {
+                string Weather = openFileData.Lines[1].ToString();
+
+                if (Weather == "Sunny") radioButton1.Checked = true;
+                else if (Weather == "Rainy") radioButton2.Checked = true;
+                else if (Weather == "Snow") radioButton3.Checked = true;
+
+            }
+
+            // 날짜 설정
+            {
+                string Date = openFileData.Lines[2].ToString();
+
+                int Year = Int32.Parse(Date.Substring(0, 4));
+                int Month = Int32.Parse(Date.Substring(5, 2));
+                int Day = Int32.Parse(Date.Substring(8, 2));
+
+                dateTimePicker1.Value = new DateTime(Year, Month, Day);
+            }
+
+            // 글꼴
+            {
+
+            }
+        }
+
+
+        private void 새로만들기_Click(object sender, EventArgs e)
+        {
+            TitleBox.Focus();
+
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+
+            MainTextBox.Text = "";
+            TitleBox.Text = "";
+            dateTimePicker1.Value = DateTime.Now;
+        }
+    }
+
+}
